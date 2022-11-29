d634ea12a13f407280ae227a33b67968c2ba8a50
Kankit40 <Kankit40@gmail.com>
2022-11-29 15:29:25 +0530

    UC1: Count the frequency and create BST

--- src/com/bridgelabz_BST/BinarySearchTree.java
+++ src/com/bridgelabz_BST/BinarySearchTree.java
@@ -1,4 +1,40 @@
 package com.bridgelabz_BST;
 
 public class BinarySearchTree {
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    public TreeNode insert( TreeNode root, int value){
+        if (root == null || root.data == value){
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
+
+    }
 }
